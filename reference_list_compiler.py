674ff27b651b1e22776274766f45ec875dc76fee
ahamilton144 <ahamilton144@gmail.com>
2020-01-22 23:41:27 -0500

    sort tags

--- reference_list_compiler.py
+++ reference_list_compiler.py
@@ -1,10 +1,4 @@
-import numpy as np
-import pandas as pd
 import bibtexparser
-from bibtexparser.bparser import BibTexParser
-from bibtexparser.customization import homogenize_latex_encoding
-from bibtexparser.customization import convert_to_unicode
-
 
 # name of bib file (without extension)
 bib_file = 'references_from_mendeley_example'
@@ -16,7 +10,7 @@ with open(bib_file + '.bib', 'r', encoding='utf8') as bibtex_file:
   bib_dict = bibtexparser.load(bibtex_file).entries_dict
 
 n_bib = len(bib_dict)
-keys_bib = list(bib_dict.keys())
+keys_bib = sorted(list(bib_dict.keys()), key=lambda v: v.upper())
 
 # Parse lists for author, tags, title
 def get_attribute(key_bib, key_attribute, parse='na'):
@@ -50,7 +44,27 @@ for i,key_bib in enumerate(keys_bib):
         tags_dict[tag] = [key_bib]
   except:
     no_tags.append(key_bib)
-keys_tag = list(tags_dict.keys())
+keys_tag = sorted(list(tags_dict.keys()), key=lambda v: v.upper())
+
+
+### function for writing ref info to file
+def write_ref(write_file, bib_dict, key_bib):
+  write_file.write("\\noindent\\citep{" + bib_dict[key_bib]['ID'] + "} \n\\begin{itemize} \n")
+  write_file.write("\\item{\\textit{Reference ID}:  " + bib_dict[key_bib]['ID'] + "} \n\n")
+  if 'author' in bib_dict[key_bib]:
+    write_file.write("\\item{\\textit{Author}:  " + bib_dict[key_bib]['author'].replace(' and ', ' $\\backslash$$\\backslash$ ') + "} \n\n")
+  if 'title' in bib_dict[key_bib]:
+    write_file.write("\\item{\\textit{Title}:  " + bib_dict[key_bib]['title'] + "} \n\n")
+  if 'journal' in bib_dict[key_bib]:
+    write_file.write("\\item{\\textit{Journal}:  " + bib_dict[key_bib]['journal'] + "} \n\n")
+  if 'year' in bib_dict[key_bib]:
+    write_file.write("\\item{\\textit{Year}:  " + bib_dict[key_bib]['year'] + "} \n\n")
+  if 'mendeley-tags' in bib_dict[key_bib]:
+    write_file.write("\\item{\\textit{Tags}:  " + bib_dict[key_bib]['mendeley-tags'].replace(',', ' $\\backslash$$\\backslash$ ') + "} \n\n")
+  if 'annote' in bib_dict[key_bib]:
+    write_file.write("\\item{\\textit{Notes}:  " + bib_dict[key_bib]['annote'] + "} \n\n")
+  write_file.write("\\end{itemize}\medskip\n\n\n\n")
+
 
 ### write latex file
 with open(latex_file + '.tex', 'w', encoding='utf8') as write_file:
@@ -67,44 +81,16 @@ with open(latex_file + '.tex', 'w', encoding='utf8') as write_file:
     write_file.write("\\noindent " + key_tag + "\\\\\n")
   write_file.write("\medskip\n\n\n\n")
   for i,key_tag in enumerate(keys_tag):
-    write_file.write("\\subsection{" + key_tag + "} \n")
+    write_file.write("\\subsection{Tag: " + key_tag + "} \n")
     for j,key_bib in enumerate(tags_dict[key_tag]):
-      write_file.write("\\noindent\\citep{" + bib_dict[key_bib]['ID'] + "} \n\\begin{itemize} \n")
-      write_file.write("\\item{Reference ID:  " + bib_dict[key_bib]['ID'] + "} \n\n")
-      if 'author' in bib_dict[key_bib]:
-        write_file.write("\\item{Authors:  " + bib_dict[key_bib]['author'] + "} \n\n")
-      if 'title' in bib_dict[key_bib]:
-        write_file.write("\\item{Title:  " + bib_dict[key_bib]['title'] + "} \n\n")
-      if 'journal' in bib_dict[key_bib]:
-        write_file.write("\\item{Journal:  " + bib_dict[key_bib]['journal'] + "} \n\n")
-      if 'year' in bib_dict[key_bib]:
-        write_file.write("\\item{Year:  " + bib_dict[key_bib]['year'] + "} \n\n")
-      if 'mendeley-tags' in bib_dict[key_bib]:
-        write_file.write("\\item{Tags:  " + bib_dict[key_bib]['mendeley-tags'] + "} \n\n")
-      if 'annote' in bib_dict[key_bib]:
-        write_file.write("\\item{Notes:  " + bib_dict[key_bib]['annote'] + "} \n\n")
-      write_file.write("\\end{itemize}\medskip\n\n\n\n")
+      write_ref(write_file, bib_dict, key_bib)
 
   write_file.write("%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%\n")
   write_file.write("%%%%%%%%%% Tagless refs ########################## %%%%%%%%%%\n")
   write_file.write("%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%\n")
   write_file.write("\\section{Tagless} \n")
   for key_bib in no_tags:
-    write_file.write("\\noindent\\citep{" + bib_dict[key_bib]['ID'] + "} \n\\begin{itemize} \n")
-    write_file.write("\\item{Reference ID:  " + bib_dict[key_bib]['ID'] + "} \n\n")
-    if 'author' in bib_dict[key_bib]:
-      write_file.write("\\item{Authors:  " + bib_dict[key_bib]['author'] + "} \n\n")
-    if 'title' in bib_dict[key_bib]:
-      write_file.write("\\item{Title:  " + bib_dict[key_bib]['title'] + "} \n\n")
-    if 'journal' in bib_dict[key_bib]:
-      write_file.write("\\item{Journal:  " + bib_dict[key_bib]['journal'] + "} \n\n")
-    if 'year' in bib_dict[key_bib]:
-      write_file.write("\\item{Year:  " + bib_dict[key_bib]['year'] + "} \n\n")
-    if 'mendeley-tags' in bib_dict[key_bib]:
-      write_file.write("\\item{Tags:  " + bib_dict[key_bib]['mendeley-tags'] + "} \n\n")
-    if 'annote' in bib_dict[key_bib]:
-      write_file.write("\\item{Notes:  " + bib_dict[key_bib]['annote'] + "} \n\n")
-    write_file.write("\\end{itemize}\medskip\n\n\n\n")
+    write_ref(write_file, bib_dict, key_bib)
 
   write_file.write("\n\\bibliography{" + bib_file + "} \n\\end{document}")
 
